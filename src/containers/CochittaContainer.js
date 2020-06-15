cee93d91b3baccfc7185231e19648b5d7c76c39e
MonksAndNinjas <blackbeltdiaries@gmail.com>
2020-06-15 16:59:26 -0600

    define art
    added variables for defining art categories which will be transferred down to art component

--- src/containers/CochittaContainer.js
+++ src/containers/CochittaContainer.js
@@ -2,21 +2,29 @@ import React from "react";
 import "../css/CochittaContainer.css";
 // connects to store and allows use of functiuons in /actions
 import { connect } from "react-redux";
+// allows use of helper function
+import { displayArt } from "../helpers/displayArt";
 
 // Upload and render artwork
 
 class CochittaContainer extends React.Component {
   render() {
+    let digital = this.props.artCollection.artCollection.digital;
+    let drawings = this.props.artCollection.artCollection.drawings;
+    let signature = this.props.artCollection.artCollection.signature;
+    let paintings = this.props.artCollection.artCollection.paintings;
+
     return (
       <div>
         <h1>Cochitta</h1>
+
+        {displayArt(this.props.artCollection) ? <div>yes</div> : null}
       </div>
     );
   }
 }
 // makes photos from store accessible
 const mapStateToProps = state => {
-  console.log(state);
   return {
     artCollection: state.artCollection
   };
